c65c24f31855ccc7f2811c5f81fd281553e5bee5
ZbrDeev <github.cannot978@passinbox.com>
2025-11-08 10:37:10 +0100

    Fix some problem in init template

--- bin/dartshine.dart
+++ bin/dartshine.dart
@@ -4,6 +4,6 @@ Future<void> main(List<String> args) async {
   if (args[0] == 'create' && args.length == 2) {
     await createProject(args[1]);
   } else if (args[0] == 'version') {
-    print("Dartshine 2.3.7");
+    print("Dartshine 2.3.8");
   }
 }
